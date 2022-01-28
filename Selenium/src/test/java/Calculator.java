99a723616ab4ced046dc992161f8c93fb09fa2f3
wakhre.ritukishor <wakhre.ritukishor@LP-5CD141BBX3.HCLT.CORP.HCL.IN>
2022-01-28 10:14:18 +0530

    First_commit

---
+++ Selenium/src/test/java/Calculator.java
@@ -0,0 +1,11 @@
+
+public class Calculator {
+	public int add1(int a,int b) {
+		return a+b;
+		
+		}
+		public int sub1(int a,int b) {
+			return a-b;
+	}
+
+}
---
+++ Selenium/src/test/java/CalculatorTest.java
@@ -0,0 +1,14 @@
+import org.testng.annotations.Test;
+
+public class CalculatorTest {
+
+  @Test
+  public void add1Test() {
+    throw new RuntimeException("Test not implemented");
+  }
+
+  @Test
+  public void sub1Test() {
+    throw new RuntimeException("Test not implemented");
+  }
+}
